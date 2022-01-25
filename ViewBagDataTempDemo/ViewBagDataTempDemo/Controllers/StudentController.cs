e1f7a0fb4eb13a893405e77985bfd2954c9308f9
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-25 18:19:36 +0530

    pushing new project

---
+++ ViewBagDataTempDemo/ViewBagDataTempDemo/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBagDataTempDemo.Models;
+namespace ViewBagDataTempDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> studentList = new List<Student>
+            {
+                new Student {StdRollNo=100,StdName="Scott"},
+                 new Student {StdRollNo=101,StdName="Tiger"},
+                 new Student{StdRollNo=101,StdName="Smith"}
+            };
+            ViewData["students"] = studentList;
+            ViewData["count"] = studentList.Count;
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+          var result=  ViewData["students"];
+            var  result1 = ViewBag.students;
+            return new EmptyResult();
+        }
+
+        public ActionResult ShowStudents()
+        {
+            List<Student> studentList = new List<Student>
+            {
+                new Student {StdRollNo=100,StdName="Scott"},
+                 new Student {StdRollNo=101,StdName="Tiger"},
+                 new Student{StdRollNo=101,StdName="Smith"}
+            };
+            ViewBag.students = studentList;
+            ViewBag.count = studentList.Count;
+            return View();
+        }
+
+        public ActionResult ShowStudentInfo()
+        {
+            TempData["rollno"] = 100;
+            return RedirectToAction("PrintRollNo");
+        }
+
+        public ActionResult PrintRollNo()
+        {
+            TempData.Keep("rollno");
+            return View();
+        }
+    }
+}
---
+++ ViewBagDataTempDemo/ViewBagDataTempDemo/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewBagDataTempDemo.Models
+{
+    public class Student
+    {
+        public int StdRollNo { get; set; }
+        public string StdName { get; set; }
+    }
+}
